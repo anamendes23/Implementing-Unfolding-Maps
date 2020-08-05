db48147dc77407e0455fb699c2d32c3a034869fb
anamendes23 <anamendes23@gmail.com>
2020-08-04 21:23:51 -0700

    created VisualClock class and added background image

---
+++ week 2/OOPStarterCode/UCSDUnfoldingMaps/src/module2/VisualClock.java
@@ -0,0 +1,20 @@
+package module2;
+
+import processing.core.*;
+
+public class VisualClock extends PApplet{
+
+    private String url = "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&url=https%3A%2F%2Fimg1.coastalliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_inbody_900x506%2Fpublic%2Fimage%2F2016%2F07%2Fmain%2Fla-jolla-cove-best-beaches-san-diego-gettyimages-632168041.jpg%3Fitok%3DUnhw14EW";
+    private PImage backgroundImg;
+
+    public void setup(){
+        size(900,506);
+        backgroundImg = loadImage(url, "jpg");
+    }
+
+    public void draw() {
+        background(0);
+        image(backgroundImg, 0,0);
+    }
+
+}
