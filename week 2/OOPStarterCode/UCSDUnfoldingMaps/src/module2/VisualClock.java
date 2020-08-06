59062790dc317ba88872e505781299baadc69801
anamendes23 <anamendes23@gmail.com>
2020-08-05 22:04:33 -0700

    added methods with two different strategies to change sun color based on the time

--- week 2/OOPStarterCode/UCSDUnfoldingMaps/src/module2/VisualClock.java
+++ week 2/OOPStarterCode/UCSDUnfoldingMaps/src/module2/VisualClock.java
@@ -7,36 +7,55 @@ public class VisualClock extends PApplet{
 
     private String url = "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&url=https%3A%2F%2Fimg1.coastalliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_inbody_900x506%2Fpublic%2Fimage%2F2016%2F07%2Fmain%2Fla-jolla-cove-best-beaches-san-diego-gettyimages-632168041.jpg%3Fitok%3DUnhw14EW";
     private PImage backgroundImg;
-    private int red;
-    private int green;
-    private int blue;
+    private int[] colors;
 
     public void setup(){
         size(500,400);
+        background(0);
         backgroundImg = loadImage(url, "jpg");
         backgroundImg.resize(0,height);
+        image(backgroundImg, 0,0);
+        colors = new int[3];
     }
 
     public void draw() {
-        background(0);
-        image(backgroundImg, 0,0);
         drawSmileyFace();
-        updateColors();
+        //updateColors();
+        colors = sunColorSet(hour());
         drawSun();
     }
 
+    // method that changes the color of the sun based on the hour of the day
+    //the color ranges from yellow to light blue
     private void updateColors() {
         //get time stamp
         int time = LocalDateTime.now().getHour();
         //color goes from yellow(255,209,0) to grey(183,185,192);
-        red = 255 - time * 3;
-        green = 209- time;
-        blue = time * 8;
+        colors[0] = 255 - time * 3;
+        colors[1] = 209- time;
+        colors[2] = time * 8;
+    }
+
+    //this method also changes the color of the sun based on time, but
+    //the method changes the brightness of the color to achieve a darker tone
+    private int[] sunColorSet(float hour) {
+        int[] rgb = new int[3];
+        //scale the brightness of the yellow based on the seconds.
+        //30 seconds is black. 0 seconds is bright yellow
+        //float diffFrom30 = Math.abs(30 - seconds);
+        float diffFrom24 = Math.abs(24 - hour);
+
+        float ratio = diffFrom24/24;
+        rgb[0] = (int)(255*ratio);
+        rgb[1] = (int)(255*ratio);
+        rgb[2] = 0;
+
+        return rgb;
     }
 
     private void drawSun() {
-        fill(red,green,blue);
-        stroke(red,green,blue);
+        fill(colors[0],colors[1],colors[2]);
+        stroke(colors[0],colors[1],colors[2]);
         ellipse((width/4)*3.3f, height/5, width/6, height/5);
     }
 
